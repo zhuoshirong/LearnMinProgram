ca6807bf11b6453d0c57db6c80516c9a86144b34
zhuosr <415382863@qq.com>
2019-11-06 16:04:02 +0800

    提交代码20191106

---
+++ componets/my_cpn/my_cpn.js
@@ -0,0 +1,23 @@
+// componets/my_cpn/my_cpn.js
+Component({
+  /**
+   * 组件的属性列表
+   */
+  properties: {
+
+  },
+
+  /**
+   * 组件的初始数据
+   */
+  data: {
+    content:'我是自定义组件内容'
+  },
+
+  /**
+   * 组件的方法列表
+   */
+  methods: {
+
+  }
+})
--- pages/personal/personal.js
+++ pages/personal/personal.js
@@ -7,60 +7,30 @@ Page({
   data: {
 
   },
-
-  /**
-   * 生命周期函数--监听页面加载
-   */
-  onLoad: function (options) {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function () {
-
-  },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function () {
-
-  },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function () {
-
-  },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function () {
+  onLoad:function(otpions){
+    // 发送网络请求
+    // https://cnkmprice.kmzyw.com.cn/pageIndex.action?loadType=11
+    // https://www.kmzyw.com.cn/bzjsp/app_remote/app_info_index_site_list.jsp
+   
+    // wx.request({
+    //   url: 'https://cnkmprice.kmzyw.com.cn/pageIndex.action?loadType=11',
+    //   success:function(res){
+    //     console.log(res);
+    //   }
+    // })
+    wx.request({
+      url: 'http://httpbin.org/post',
+      method : 'post',
+      data :{
+        name:'coderwhy',
+        age:25
+      }
+      ,
+      success:function(res){
+          console.log(res)
+      }
+    })
 
   }
+ 
 })
